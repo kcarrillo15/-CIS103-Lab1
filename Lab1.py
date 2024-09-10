7d10ad9cf05bb4e078e24ee9f42db86bbe7017fb
Kevin <kcarrillo15@student.ccc.edu>
2024-09-10 00:19:23 -0500

    H

---
+++ Lab1.py
@@ -0,0 +1,10 @@
+# Lab 1: Getting Started with Python
+# CIS 103: Introduction to Programming
+# Instructor: Md Ali
+# Student Name: Kevin Carrillo
+# Date: 9/9/2024
+# Description:
+# This script prints a greeting message to the user.
+# --------------------------------------------------------
+# The following line of code prints a greeting message
+print("Hello, welcome to CIS 103!")
