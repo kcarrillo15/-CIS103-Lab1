4ca87ad7e8a1a7f8b3f8c6941dd8264fda5a6a87
Kevin <kcarrillo15@student.ccc.edu>
2024-09-10 00:20:40 -0500

    I had some errors trying to commit this

--- Lab1.py
+++ Lab1.py
@@ -1,10 +1,18 @@
+# --------------------------------------------------------
 # Lab 1: Getting Started with Python
 # CIS 103: Introduction to Programming
-# Instructor: Md Ali
-# Student Name: Kevin Carrillo
-# Date: 9/9/2024
+# Instructor: [Your Name]
+# Student Name: [Your Name]
+# Date: [Today's Date]
 # Description:
-# This script prints a greeting message to the user.
+# This script prints a personalized greeting message
+# and demonstrates the use of variables and basic data types.
 # --------------------------------------------------------
-# The following line of code prints a greeting message
-print("Hello, welcome to CIS 103!")
+# Get the user's name (string) and age (integer)
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+# Calculate the year the user was born
+current_year = 2024
+birth_year = current_year - age
+# Print a personalized greeting message
+print(f"Hello, {name}! You were born in {birth_year}.")
